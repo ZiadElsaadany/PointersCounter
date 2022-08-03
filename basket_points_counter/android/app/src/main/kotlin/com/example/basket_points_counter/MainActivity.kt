68335126be2df7342b7901a08c7e68ab617876cc
ZiadElsaadany <ziadelsaadany73@gmail.com>
2022-08-04 00:59:57 +0200

    logic and navigator and validate

---
+++ basket_points_counter/android/app/src/main/kotlin/com/example/basket_points_counter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.basket_points_counter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ basket_points_counter/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:basket_points_counter/Home.dart';
+import 'package:basket_points_counter/info.dart';
+import 'package:flutter/material.dart';
+
+void main ( )  {
+
+  runApp(BasketPointsCounter());
+
+}
+class BasketPointsCounter extends StatelessWidget {
+  const BasketPointsCounter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false ,
+      home: FirstScreen(),
+      routes: {
+        Home.route_name:(context)=>Home() ,
+        FirstScreen.route_name:(context)=>FirstScreen() ,
+      },
+    );
+  }
+}
